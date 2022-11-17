b137d9538f413fcedb8a8b30220cd124f9c85ab6
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-11-17 09:37:41 +0100

    ajout des classes utiles

---
+++ src/activeRecord/Film.java
@@ -0,0 +1,34 @@
+package activeRecord;
+
+public class Film {
+    public Film(String seven, Personne p2) {
+    }
+
+    public static void createTable() {
+    }
+
+    public static Film findById(int i) {
+        return null;
+    }
+
+    public static void deleteTable() {
+    }
+
+    public void save() {
+    }
+
+    public boolean getId() {
+        return false;
+    }
+
+    public boolean getTitre() {
+        return false;
+    }
+
+    public Personne getRealisateur() {
+        return null;
+    }
+
+    public void setTitre(String test2) {
+    }
+}
---
+++ src/activeRecord/Personne.java
@@ -0,0 +1,47 @@
+package activeRecord;
+
+import java.util.ArrayList;
+
+public class Personne {
+    public Personne(String spielberg, String steven) {
+    }
+
+    public static void createTable() {
+    }
+
+    public static ArrayList<Personne> findAll() {
+        return null;
+    }
+
+    public static Personne findById(int i) {
+        return null;
+    }
+
+    public static ArrayList<Personne> findByName(String fincher) {
+        return null;
+    }
+
+    public static void deleteTable() {
+    }
+
+    public void save() {
+    }
+
+    public void delete() {
+    }
+
+    public void setNom(String f_i_n_c_h_e_r) {
+    }
+
+    public void setPrenom(String davif) {
+    }
+
+    public void getId() {
+    }
+
+    public void getNom() {
+    }
+
+    public void getPrenom() {
+    }
+}
