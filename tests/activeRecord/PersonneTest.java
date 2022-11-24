01348e177e28acc3f48086367d5e40bac9020562
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-11-24 09:42:08 +0100

    fin des tests Personne

--- tests/activeRecord/PersonneTest.java
+++ tests/activeRecord/PersonneTest.java
@@ -12,6 +12,10 @@ class PersonneTest {
     void setUp() {
         //on cree la table
         Personne.createTable();
+        Personne p1 = new Personne("Spielberg", "Steven");
+        p1.save();
+        Personne p2 = new Personne("Scott", "Ridley");
+        p2.save();
     }
 
     @AfterEach
@@ -21,45 +25,54 @@ class PersonneTest {
     }
 
     @Test
-    void createTable() {
-        //cas ou la table existe deja
-        //la methode ne doit pas lever d'exception
-        Personne.createTable();
-        //cas ou la table n'existe pas
+    void findAll() {
+        //cas ou la table contient des elements
+        assertEquals(2, Personne.findAll().size());
+        //cas ou la table est vide
         Personne.deleteTable();
         Personne.createTable();
-    }
-
-    @Test
-    void findAll() {
+        assertEquals(0, Personne.findAll().size());
     }
 
     @Test
     void findById() {
+        //cas ou l'id existe
+        assertEquals("Spielberg", Personne.findById(1).getNom());
+        //cas ou l'id n'existe pas
+        assertNull(Personne.findById(3));
+        //cas ou l'id est negatif
+        assertNull(Personne.findById(-1));
+        //cas ou l'id est nul
+        assertNull(Personne.findById(0));
+        //cas ou la table est vide
+        Personne.deleteTable();
+        Personne.createTable();
+        assertNull(Personne.findById(1));
     }
 
     @Test
     void findByName() {
-    }
-
-    @Test
-    void deleteTable() {
-    }
-
-    @Test
-    void save() {
+        //cas ou le nom existe
+        assertEquals(1, Personne.findByName("Ridley").size());
+        //cas ou le nom n'existe pas
+        assertEquals(0, Personne.findByName("crashtest").size());
+        //cas ou la table est vide
+        Personne.deleteTable();
+        Personne.createTable();
+        assertEquals(0, Personne.findByName("Ridley").size());
     }
 
     @Test
     void delete() {
-    }
-
-    @Test
-    void setNom() {
-    }
-
-    @Test
-    void setPrenom() {
+        //cas ou l'id existe
+        Personne.findById(1).delete();
+        assertEquals(1, Personne.findAll().size());
+        //cas ou l'id n'existe pas
+        assertThrows(NullPointerException.class, () -> Personne.findById(3).delete());
+        //cas ou la table est vide
+        Personne.deleteTable();
+        Personne.createTable();
+        assertThrows(NullPointerException.class, () -> Personne.findById(1).delete());
     }
 
 }
